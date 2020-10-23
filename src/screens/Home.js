eac33acdd2c0fbd9de16689233c3783c1a8638a1
Oleh Makarenko <olegomm@gmail.com>
2020-10-23 17:49:00 +0300

    create pluginlist. Mock home notifications

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { 
 	Section, 
 	GradientBackground,
 	BalanceWidget,
-	Text
+	Text,
+	PluginList,
+	Col,
+	Row
 } from '@src/components';
 import translate from "@src/locales/i18n";
 import { Router } from "@src/Router";
 import store from '@src/store';
 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	transactionPreview: {
+		width: '100%',
+		height: 60,
+		borderRadius: 6,
+		marginTop: 4,
+		marginBottom: 4,
+		padding: 17,
+		paddingTop: 8,
+		backgroundColor: '#fffc'
+	},
+	list: {
+		marginTop: 17,
+		marginBottom: 80
+	}
+});
 
 type Props = {};
 
@@ -33,9 +51,35 @@ export default class Home extends Component<Props, State> {
 				<Section type="title">
 					<Text type='subtitle'>Switch account button, qr, etc..</Text>
 				</Section>
-				<Section type="center">
-					<BalanceWidget/>
-				</Section>
+				<Col justify="space-around" style={{flex: 1}}>
+					<Section type="center">
+						<BalanceWidget/>
+					</Section>
+					<Section type="list">
+						<PluginList />					
+					</Section>
+					<Section type="list" style={styles.list}>
+						{/* TODO: move transaction preview to a separate component. Mockup */}
+						<View style={styles.transactionPreview}>
+							<Row justify="space-between">
+								<Text type="regular" theme="light">Opt-in</Text>
+								<Text type="regular" theme="light">23.10.2020 11:00</Text>
+							</Row>
+							<Row justify="space-between">
+								<Text type="bold" theme="light">Post launch Opt-in</Text>
+							</Row>
+						</View>
+						<View style={styles.transactionPreview}>
+						<Row justify="space-between">
+								<Text type="regular" theme="light">Multisig Transaction</Text>
+								<Text type="regular" theme="light">23.10.2020 10:59</Text>
+							</Row>
+							<Row justify="space-between">
+								<Text type="bold" theme="light">Awaiting your signature</Text>
+							</Row>
+						</View>
+					</Section>
+				</Col>
 			</GradientBackground>
         );
     };
