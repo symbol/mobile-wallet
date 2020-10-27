41d9b7274212e225513acf6c4a69f3891bbae235
Oleh Makarenko <olegomm@gmail.com>
2020-10-27 18:43:54 +0200

    feed Send form with state

--- src/screens/Send.js
+++ src/screens/Send.js
@@ -31,11 +31,25 @@ type State = {};
 
 
 class Send extends Component<Props, State> {
-	state = {};
+	state = {
+		recipientAddress: '',
+		mosaicNamespaceName: 'Symbol.XYM',
+		amount: '0',
+		message: '',
+		isEncrypted: false,
+		fee: '0.5'
+	};
 
-    render() {
+    render = () => {
 		const {} = this.props;
-		const {} = this.state;
+		const {
+			recipientAddress,
+			mosaicNamespaceName,
+			amount,
+			message,
+			isEncrypted,
+			fee
+		} = this.state;
 
         return (
 			<GradientBackground name="connector_small" theme="light">
@@ -47,23 +61,43 @@ class Send extends Component<Props, State> {
 					<Section type="form" style={styles.list} isScrollable>
 						<Section type="form-item">
 							<Input 
-								value="TDHGSJHDHJJDJ"
+								value={recipientAddress}
 								placeholder="Recipient Address"
 								theme="light"
+								onChangeText={recipientAddress => this.setState({recipientAddress})}
 							/>
 						</Section>
 						<Section type="form-item">
 							<Input 
-								value="TDHGSJHDHJJDJ"
-								placeholder="Recipient Address"
+								value={mosaicNamespaceName}
+								placeholder="Mosaic"
 								theme="light"
+								editable={false}
+								onChangeText={mosaicNamespaceName => this.setState({mosaicNamespaceName})}
 							/>
 						</Section>
 						<Section type="form-item">
 							<Input 
-								value="TDHGSJHDHJJDJ"
-								placeholder="Recipient Address"
+								value={amount}
+								placeholder="Amount"
 								theme="light"
+								onChangeText={amount => this.setState({amount})}
+							/>
+						</Section>
+						<Section type="form-item">
+							<Input 
+								value={message}
+								placeholder="Message / Memo"
+								theme="light"
+								onChangeText={message => this.setState({message})}
+							/>
+						</Section>
+						<Section type="form-item">
+							<Input 
+								value={fee}
+								placeholder="Fee"
+								theme="light"
+								onChangeText={fee => this.setState({fee})}
 							/>
 						</Section>
 						<Section type="form-bottom">
