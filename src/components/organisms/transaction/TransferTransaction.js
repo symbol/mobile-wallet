87b9f39e9734accb823151669841cfec4d8b92dc
AdriaCarrera <adria.cm95@gmail.com>
2020-11-08 01:02:34 +0100

    Rolled back transaction skeleton on history screen

--- src/components/organisms/transaction/TransferTransaction.js
+++ src/components/organisms/transaction/TransferTransaction.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
-import { View, Image, StyleSheet, Linking } from 'react-native';
-import { Text, Row } from '@src/components';
+import { View, Image, StyleSheet, Linking, TouchableOpacity } from 'react-native';
+import { Text, Row, Button } from '@src/components';
 import type { TransactionModel, TransferTransactionModel } from '@src/storage/models/TransactionModel';
 import { connect } from 'react-redux';
 
 const styles = StyleSheet.create({
-    transactionPreview: {
+    root: {
         width: '100%',
-        height: 60,
         borderRadius: 6,
-        marginTop: 4,
-        marginBottom: 4,
+        marginTop: 0,
+        marginBottom: 8,
         padding: 17,
         paddingTop: 8,
-        backgroundColor: '#fff5',
+        paddingBottom: 8,
+        backgroundColor: '#fffd',
     },
 });
 
 type Props = {
     transaction: TransferTransactionModel,
+    showDetails: boolean,
 };
 
 class TransferTransaction extends Component<Props> {
     render() {
-        const { transaction, network } = this.props;
+        const { transaction, network, showDetails, openExplorer } = this.props;
         const currencyMosaic = transaction.mosaics.find(mosaic => mosaic.mosaicId === network.currencyMosaicId);
         return (
-            <View style={styles.transactionPreview}>
+            <View style={styles.root}>
                 <Row justify="space-between">
                     <Text type="regular" theme="light">
                         {transaction.type}
@@ -40,9 +41,54 @@ class TransferTransaction extends Component<Props> {
                         {transaction.signerAddress.slice(0, 9)}...
                     </Text>
                     <Text type="bold" theme="light">
-                        Amount: {currencyMosaic ? (currencyMosaic.amount / Math.pow(10, currencyMosaic.divisibility)) : 0}
+                        Amount: {currencyMosaic ? currencyMosaic.amount / Math.pow(10, currencyMosaic.divisibility) : 0}
                     </Text>
                 </Row>
+                {showDetails && (
+                    <View>
+                        <Row justify="space-between">
+                            <Text type="bold" theme="light">
+                                Mosaics:
+                            </Text>
+                        </Row>
+                        {transaction.mosaics.map(mosaic => {
+                            return (
+                                <Row justify="space-between">
+                                    <Text type="regular" theme="light" align="right">
+                                        {mosaic.mosaicName || mosaic.mosaicId}
+                                    </Text>
+                                    <Text type="regular" theme="light">
+                                        {mosaic.amount / Math.pow(10, mosaic.divisibility)}
+                                    </Text>
+                                </Row>
+                            );
+                        })}
+                        <Row justify="space-between">
+                            <Text type="bold" theme="light">
+                                Message:
+                            </Text>
+                            <Text type="bold" theme="light" onClick>
+                                {transaction.messageEncrypted ? 'Encrypted' : transaction.messageText}
+                            </Text>
+                        </Row>
+                        <Row justify="space-between">
+                            <Text type="bold" theme="light">
+                                Hash:
+                            </Text>
+                            <Text type="regular" theme="light">
+                                {transaction.hash.slice(0, 24)}...
+                            </Text>
+                        </Row>
+
+                        <TouchableOpacity onPress={openExplorer}>
+                            <Row justify="end">
+                                <Text type="regular" theme="light">
+                                    Open in the explorer
+                                </Text>
+                            </Row>
+                        </TouchableOpacity>
+                    </View>
+                )}
             </View>
         );
     }
--- src/screens/History.js
+++ src/screens/History.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { Section, ImageBackground, Text, Row, TitleBar } from '@src/components';
 import Transaction from '@src/components/organisms/transaction/Transaction';
 import { connect } from 'react-redux';
 import store from '@src/store';
 
 const styles = StyleSheet.create({
+    list: {
+        marginBottom: 70,
+    },
 });
 
 type Props = {};
@@ -13,25 +16,44 @@ type Props = {};
 type State = {};
 
 class History extends Component<Props, State> {
-    state = {};
+    state = {
+        showingDetails: -1,
+    };
 
     componentDidMount() {
-		store.dispatchAction({ type: 'account/loadTransactions' });
-		this.props.dataManager.reset();
+        store.dispatchAction({ type: 'account/loadTransactions' });
+        this.props.dataManager.reset();
+    }
+
+    showDetails = index => {
+        const { showingDetails } = this.state;
+        if (showingDetails === index) {
+            this.setState({
+                showingDetails: -1,
+            });
+        } else {
+            this.setState({
+                showingDetails: index,
+            });
+        }
     }
 
     render() {
         const { dataManager } = this.props;
-		const {} = this.state;
-		const transactions = dataManager.data;
+        const { showingDetails } = this.state;
+        const transactions = dataManager.data;
 
         return (
             <ImageBackground name="tanker" dataManager={dataManager}>
                 <TitleBar theme="light" title="Transactions" />
-                <Section type="list" isScrollable>
+                <Section type="list" style={styles.list} isScrollable>
                     {transactions &&
-                        transactions.map(tx => {
-                            return <Transaction transaction={tx} />;
+                        transactions.map((tx, index) => {
+                            return (
+                                <TouchableOpacity onPress={() => this.showDetails(index)}>
+                                    <Transaction transaction={tx} showDetails={showingDetails === index} />
+                                </TouchableOpacity>
+                            );
                         })}
                 </Section>
             </ImageBackground>
@@ -40,5 +62,5 @@ class History extends Component<Props, State> {
 }
 
 export default connect(state => ({
-	dataManager: state.account.transactionListManager,
+    dataManager: state.account.transactionListManager,
 }))(History);
