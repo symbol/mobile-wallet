9e69e48cf9bb146f58c1d8982d1eb6011c005540
Oleh Makarenko <olegomm@gmail.com>
2020-11-09 23:10:30 +0200

    add clipboard copy/paste functions to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import Clipboard from '@react-native-community/clipboard';
+
 export const getDropdownListFromObjct = 
 	obj => Object
 		.keys(obj)
@@ -5,3 +7,15 @@ export const getDropdownListFromObjct =
 			value: el,
 			label: obj[el]
 		}));
+
+export const pasteFromClipboard = async () => {
+	try {
+		const text = await Clipboard.getString();
+		return text; 
+	}
+	catch(e){ return ''; }
+};
+
+export const copyToClipboard = (str: string) => {
+    Clipboard.setString(str);
+};
