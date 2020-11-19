164331c12cf71f613443ba83e121eaa655896760
AdriaCarrera <adria.cm95@gmail.com>
2020-11-19 11:27:44 +0100

    Globe Icon not working

--- src/screens/ConfirmTransaction.js
+++ src/screens/ConfirmTransaction.js
@@ -1,18 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, TouchableOpacity, Linking } from 'react-native';
-import {
-	Section,
-	GradientBackground,
-	TitleBar,
-	Text,
-	Button,
-	TableView,
-	LinkExplorer,
-	Icon,
-	Row,
-	Col
-} from '@src/components';
-import translate from "@src/locales/i18n";
+import { Section, GradientBackground, TitleBar, Text, Button, TableView, LinkExplorer, Icon, Row, Col } from '@src/components';
+import translate from '@src/locales/i18n';
 import Store from '@src/store';
 import { Router } from '@src/Router';
 import { connect } from 'react-redux';
@@ -89,71 +78,47 @@ class ConfirmTransaction extends Component<Props, State> {
         const backFunction = isSuccessfullySent
             ? () => Router.goToDashboard()
             : typeof onBack === 'function'
-                ? onBack
-                : () => Router.goBack(this.props.componentId);
+            ? onBack
+            : () => Router.goBack(this.props.componentId);
 
         return (
-			<GradientBackground name="connector" theme="light" dataManager={hardCodedDataManager} onBack={onBack}>
-				<TitleBar
-					theme="light"
-					onBack={backFunction}
-					title="Confirm Transaction"
-				/>
-				{isPreviewShown &&
-					<Section type="form" style={styles.list} isScrollable>
-						<TableView data={preview} />
-						<Section type="form-bottom">
-							<Button
-								isLoading={false}
-								isDisabled={false}
-								text="Confirm"
-								theme="light"
-								onPress={() => this.submit()}
-							/>
-						</Section>
-					</Section>
-				}
-				{isSuccessfullySent &&
-					<Section type="form">
-						<Col justify="center" style={{marginTop: '15%'}}>
-							<Section type="form-item">
-								<Row justify="space-between" align="end">
-									<Text type="alert" theme="light" style={{paddingBottom: 0}}>
-										Success!
-									</Text>
-									<Icon name="success" size="big"/>
-								</Row>
-							</Section>
-							<Section type="form-item">
-									<Text type="bold" theme="light">
-										Transaction succesfully sent. Pending to be confirmed by the network
-									</Text>
-							</Section>
+            <GradientBackground name="connector" theme="light" dataManager={hardCodedDataManager} onBack={onBack}>
+                <TitleBar theme="light" onBack={backFunction} title="Confirm Transaction" />
+                {isPreviewShown && (
+                    <Section type="form" style={styles.list} isScrollable>
+                        <TableView data={preview} />
+                        <Section type="form-bottom">
+                            <Button isLoading={false} isDisabled={false} text="Confirm" theme="light" onPress={() => this.submit()} />
+                        </Section>
+                    </Section>
+                )}
+                {isSuccessfullySent && (
+                    <Section type="form">
+                        <Col justify="center" style={{ marginTop: '15%' }}>
                             <Section type="form-item">
-                                <TouchableOpacity onPress={() => this.openExplorer()}>
-                                    <Text type="bold" theme="light" align="center">
-                                        <Icon name="explorer_light" size="small" /> Open Block Explorer
+                                <Row justify="space-between" align="end">
+                                    <Text type="alert" theme="light" style={{ paddingBottom: 0 }}>
+                                        Success!
                                     </Text>
-                                </TouchableOpacity>
+                                    <Icon name="success" size="big" />
+                                </Row>
                             </Section>
-							<Section type="form-item">
-								<LinkExplorer type="transaction" value={transaction.hash} />
-							</Section>
-						</Col>
+                            <Section type="form-item">
+                                <Text type="bold" theme="light">
+                                    Transaction succesfully sent. Pending to be confirmed by the network
+                                </Text>
+                            </Section>
+                            <Section type="form-item">
+                                <LinkExplorer type="transaction" value={transaction.hash} />
+                            </Section>
+                        </Col>
 
-
-						<Section type="form-bottom">
-							<Button
-								isLoading={false}
-								isDisabled={false}
-								text="Go to dashboard"
-								theme="light"
-								onPress={() => Router.goToDashboard()}
-							/>
-						</Section>
-					</Section>
-				}
-			</GradientBackground>
+                        <Section type="form-bottom">
+                            <Button isLoading={false} isDisabled={false} text="Go to dashboard" theme="light" onPress={() => Router.goToDashboard()} />
+                        </Section>
+                    </Section>
+                )}
+            </GradientBackground>
         );
     };
 }
