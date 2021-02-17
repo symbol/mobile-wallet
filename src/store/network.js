24625118950bab7b00ddceeb6c0f2cf2823ab943
AdriaCarrera <adria.cm95@gmail.com>
2021-02-17 17:11:58 +0100

    Fix Android "Node is down" #128

--- src/store/network.js
+++ src/store/network.js
@@ -3,12 +3,13 @@ import { getDefaultNetworkType, getNodes } from '@src/config/environment';
 import NetworkService from '@src/services/NetworkService';
 import { GlobalListener } from '@src/store/index';
 
-const NETWORK_JOB_INTERVAL = 3000;
+const NETWORK_JOB_INTERVAL = 5000;
 
 export default {
     namespace: 'network',
     state: {
         isUp: true,
+        nodeFailedAttempts: 0,
         checkNodeJob: null,
         isLoaded: false,
         generationHash: '',
@@ -53,6 +54,10 @@ export default {
         },
         setIsUp(state, payload) {
             state.network.isUp = payload;
+            return state;
+        },
+        setNodeFailedAttempts(state, payload) {
+            state.network.nodeFailedAttempts = payload;
             return state;
         },
     },
@@ -100,7 +105,15 @@ export default {
             if (selectedNetwork) {
                 try {
                     isUp = await NetworkService.isNetworkUp(selectedNetwork);
-                } catch {}
+                } catch {
+                    isUp = false;
+                }
+                if (isUp) {
+                    commit({ type: 'network/setNodeFailedAttempts', payload: 0 });
+                } else {
+                    commit({ type: 'network/setNodeFailedAttempts', payload: state.network.nodeFailedAttempts + 1 });
+                }
+                isUp = state.network.nodeFailedAttempts < 3;
             }
             commit({ type: 'network/setIsUp', payload: isUp });
         },
