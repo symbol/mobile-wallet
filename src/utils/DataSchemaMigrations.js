d57137a58d3751507dcc52f9b9c02734dfb844ea
AdriaCarrera <adria.cm95@gmail.com>
2021-03-11 09:23:23 +0100

    Save mnemonic on migration

--- src/utils/DataSchemaMigrations.js
+++ src/utils/DataSchemaMigrations.js
@@ -4,6 +4,7 @@ import Account from '@src/utils/storage/models/Account';
 import { AccountSecureStorage } from '@src/storage/persistence/AccountSecureStorage';
 import AccountService from '@src/services/AccountService';
 import { SecureStorage } from '@src/utils/storage/SecureStorage';
+import { MnemonicSecureStorage } from '@src/storage/persistence/MnemonicSecureStorage';
 
 export const CURRENT_DATA_SCHEMA = 2;
 
@@ -20,6 +21,7 @@ const migrateVersion0 = async () => {
     console.log('Migrating from version 0');
     const mnemonic = await SecureStorage.secureRetrieveAsync('mnemonics');
     if (!mnemonic) return;
+    await MnemonicSecureStorage.saveMnemonic(mnemonic);
     const accounts = await getLocalAccounts().toPromise();
     for (let account: Account of accounts) {
         const newAccountModel = AccountService.createFromMnemonicAndIndex(
