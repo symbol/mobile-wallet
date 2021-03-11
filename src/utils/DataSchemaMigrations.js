9c95aafb637e2dff4552f64e37793a1ab0326091
AdriaCarrera <adria.cm95@gmail.com>
2021-03-11 09:02:05 +0100

    Remove unused migration 1

--- src/utils/DataSchemaMigrations.js
+++ src/utils/DataSchemaMigrations.js
@@ -3,9 +3,7 @@ import { getLocalAccounts } from '@src/utils/storage/RealmDB';
 import Account from '@src/utils/storage/models/Account';
 import { AccountSecureStorage } from '@src/storage/persistence/AccountSecureStorage';
 import AccountService from '@src/services/AccountService';
-import { MnemonicSecureStorage } from '@src/storage/persistence/MnemonicSecureStorage';
-import type { AccountModel } from '@src/storage/models/AccountModel';
-import {SecureStorage} from "@src/utils/storage/SecureStorage";
+import { SecureStorage } from '@src/utils/storage/SecureStorage';
 
 export const CURRENT_DATA_SCHEMA = 2;
 
@@ -13,9 +11,6 @@ export const migrateDataSchema = async (oldVersion: number) => {
     switch (oldVersion) {
         case 0:
             await migrateVersion0();
-            break;
-        case 1:
-            await migrateVersion1();
             break;
     }
     await AsyncCache.setDataSchemaVersion(CURRENT_DATA_SCHEMA);
@@ -40,19 +35,4 @@ const migrateVersion0 = async () => {
     await AccountSecureStorage.createNewAccount(testnetAccountModel);
     const mainnetAccountModel = AccountService.createFromMnemonicAndIndex(mnemonic, 0, 'Seed account 0', 'mainnet');
     await AccountSecureStorage.createNewAccount(mainnetAccountModel);
-};
-
-const migrateVersion1 = async () => {
-    console.log('Migrating from version 1');
-    const mnemonicModel = await MnemonicSecureStorage.retrieveMnemonic();
-    const accounts = await AccountSecureStorage.getAllAccounts();
-    for (let account: AccountModel of accounts) {
-        await AccountSecureStorage.updateAccount({
-            ...account,
-            network: 'testnet',
-            type: 'privateKey',
-        });
-    }
-    const newAccountModel = AccountService.createFromMnemonicAndIndex(mnemonicModel.mnemonic, 0, 'Root account', 'mainnet');
-    await AccountSecureStorage.createNewAccount(newAccountModel);
 };
