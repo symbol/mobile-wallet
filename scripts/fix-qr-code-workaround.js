24952c29e6de9049ab22fb99b024869217b9b2b9
Oleg Makarenko <olegomm@gmail.com>
2022-01-25 22:53:25 +0200

    fix: replace paper-wallets with symbol-wallets-lib dep, update realm dep

--- scripts/fix-qr-code-workaround.js
+++ scripts/fix-qr-code-workaround.js
@@ -12,7 +12,7 @@ fs.readFile(MAIN_FILE_PATH, 'utf8', function(err, data) {
     });
 });
 
-const MODULE_FILE_PATH = __dirname + '/../node_modules/symbol-paper-wallets/node_modules/symbol-qr-library/dist/src/QRCode.js';
+const MODULE_FILE_PATH = __dirname + '/../node_modules/symbol-wallets-lib/node_modules/symbol-qr-library/dist/src/QRCode.js';
 
 if (fs.existsSync(MODULE_FILE_PATH)) {
     fs.readFile(MODULE_FILE_PATH, 'utf8', function(err, data) {
@@ -20,7 +20,7 @@ if (fs.existsSync(MODULE_FILE_PATH)) {
 
         fs.writeFile(MODULE_FILE_PATH, formatted, 'utf8', function(err) {
             if (err) return console.log(err);
-            else console.log('[+] Fixed QR Code under paper-wallets module...');
+            else console.log('[+] Fixed QR Code under symbol-wallets-lib module...');
         });
     });
 }
