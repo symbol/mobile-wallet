cc5f051137f3a27d25f702bb9d67c354002a6d38
Oleg Makarenko <olegomm@gmail.com>
2022-09-15 19:59:47 +0300

    [wallet/mobile] feat: connect store in unit tests for AddressBook screen

--- __tests__/screens/AddressBook.spec.js
+++ __tests__/screens/AddressBook.spec.js
@@ -1,53 +1,54 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
-import { AddressBookPage } from '@src/screens/AddressBook';
+import { fireEvent } from '@testing-library/react-native';
+import AddressBookPage from '@src/screens/AddressBook';
 import { Router } from '@src/Router';
-import store from '@src/store';
 import { AddressBook } from 'symbol-address-book';
+import { mockStore } from '__mocks__/store';
 
 jest.mock('@src/locales/i18n', () => t => `t_${t}`);
 
-const mockDispatchAction = jest.fn().mockResolvedValue(null);
 const mockGoToAddContact = jest.fn();
 const mockGoToContactProfile = jest.fn();
+const contacts = [
+    {
+        id: 0,
+        name: 'Rynok Pryvoz',
+        address: 'TBWKWT2HMH7ADCZO2GDZAXEXXDISOKQOFSD3YWA',
+        isBlackListed: false,
+    },
+    {
+        id: 1,
+        name: 'Frantsuzky Bulvar',
+        address: 'TAB4432XBEOOIZPVXAXKJZFULEC72FSREFVCHKY',
+        isBlackListed: true,
+    },
+    {
+        id: 2,
+        name: 'Staroportofrankivska Vulytsya',
+        address: 'TDWINZ6GNTUWNYCXZ6NJBJWMZ3QU43Q7RWK2P6I',
+        isBlackListed: false,
+    },
+];
+let store;
 
 beforeEach(() => {
-    store.dispatchAction = mockDispatchAction;
+    store = mockStore({
+        addressBook: {
+            addressBook: new AddressBook(contacts),
+            selectedContact: null,
+        },
+    });
     Router.goToAddContact = mockGoToAddContact;
     Router.goToContactProfile = mockGoToContactProfile;
-    mockDispatchAction.mockClear();
     mockGoToAddContact.mockClear();
     mockGoToContactProfile.mockClear();
 });
 
 describe('screens/AddressBook', () => {
-    // Arrange:
-    const contacts = [
-        {
-            id: 0,
-            name: 'Rynok Pryvoz',
-            address: 'TBWKWT2HMH7ADCZO2GDZAXEXXDISOKQOFSD3YWA',
-            isBlackListed: false,
-        },
-        {
-            id: 1,
-            name: 'Frantsuzky Bulvar',
-            address: 'TAB4432XBEOOIZPVXAXKJZFULEC72FSREFVCHKY',
-            isBlackListed: true,
-        },
-        {
-            id: 2,
-            name: 'Staroportofrankivska Vulytsya',
-            address: 'TDWINZ6GNTUWNYCXZ6NJBJWMZ3QU43Q7RWK2P6I',
-            isBlackListed: false,
-        },
-    ];
-    const addressBook = new AddressBook(contacts);
-
     describe('whitelist', () => {
         test('renders contact list', () => {
             // Act:
-            const screen = render(<AddressBookPage addressBook={addressBook} />);
+            const screen = renderConnected(<AddressBookPage />, store);
 
             // Assert:
             expect(screen.queryByText(contacts[0].name)).not.toBeNull();
@@ -57,16 +58,14 @@ describe('screens/AddressBook', () => {
 
         test('invoke "AddContact" screen when press on button', async () => {
             // Act:
-            const screen = render(<AddressBookPage addressBook={addressBook} />);
+            const screen = renderConnected(<AddressBookPage />, store);
             const buttonElement = screen.getByText('t_addressBook.addContact');
             fireEvent.press(buttonElement);
             await new Promise(setImmediate);
+            const selectedContact = store.getState().addressBook.selectedContact;
 
             // Assert:
-            expect(mockDispatchAction).toBeCalledWith({
-                type: 'addressBook/selectContact',
-                payload: null,
-            });
+            expect(selectedContact).toEqual(null);
             expect(mockGoToAddContact).toBeCalledWith({ isBlackListed: false }, undefined);
             expect(mockGoToContactProfile).toHaveBeenCalledTimes(0);
         });
@@ -75,7 +74,7 @@ describe('screens/AddressBook', () => {
     describe('blacklist', () => {
         test('renders blacklist', async () => {
             // Act:
-            const screen = render(<AddressBookPage addressBook={addressBook} />);
+            const screen = renderConnected(<AddressBookPage />, store);
             const tabBlacklistElement = screen.getByText('t_addressBook.blacklist');
             fireEvent.press(tabBlacklistElement);
 
@@ -87,18 +86,16 @@ describe('screens/AddressBook', () => {
 
         test('invoke "AddContact" screen when press on button', async () => {
             // Act:
-            const screen = render(<AddressBookPage addressBook={addressBook} />);
+            const screen = renderConnected(<AddressBookPage />, store);
             const tabBlacklistElement = screen.getByText('t_addressBook.blacklist');
             fireEvent.press(tabBlacklistElement);
             const buttonElement = screen.getByText('t_addressBook.addContact');
             fireEvent.press(buttonElement);
             await new Promise(setImmediate);
+            const selectedContact = store.getState().addressBook.selectedContact;
 
             // Assert:
-            expect(mockDispatchAction).toBeCalledWith({
-                type: 'addressBook/selectContact',
-                payload: null,
-            });
+            expect(selectedContact).toEqual(null);
             expect(mockGoToAddContact).toBeCalledWith({ isBlackListed: true }, undefined);
             expect(mockGoToContactProfile).toHaveBeenCalledTimes(0);
         });
@@ -106,16 +103,14 @@ describe('screens/AddressBook', () => {
 
     test('invoke "ContactProfile" screen when press on item', async () => {
         // Act:
-        const screen = render(<AddressBookPage addressBook={addressBook} />);
+        const screen = renderConnected(<AddressBookPage />, store);
         const buttonElement = screen.getByText(contacts[0].name);
         fireEvent.press(buttonElement);
         await new Promise(setImmediate);
+        const selectedContact = store.getState().addressBook.selectedContact;
 
         // Assert:
-        expect(mockDispatchAction).toBeCalledWith({
-            type: 'addressBook/selectContact',
-            payload: contacts[0],
-        });
+        expect(selectedContact).toEqual(contacts[0]);
         expect(mockGoToAddContact).toHaveBeenCalledTimes(0);
         expect(mockGoToContactProfile).toHaveBeenCalledTimes(1);
     });
