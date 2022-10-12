e24cc4a67df89976057f6b5bffbab5cf235ba952
Oleg Makarenko <olegomm@gmail.com>
2022-10-12 23:08:00 +0300

    [wallet/mobile] task: refactor SwipeablePanel component

--- src/components/molecules/SwipeablePanel.js
+++ src/components/molecules/SwipeablePanel.js
@@ -1,268 +1,92 @@
-import React, { Component } from 'react';
-import { Animated, Dimensions, PanResponder, StyleSheet, TouchableHighlight, TouchableWithoutFeedback, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { Animated, BackHandler, Dimensions, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+import { TitleBar } from '@src/components';
+import GlobalStyles from '@src/styles/GlobalStyles';
+import { useState } from 'react';
 
-let FULL_HEIGHT = Dimensions.get('window').height;
-let FULL_WIDTH = Dimensions.get('window').width;
-let PANEL_HEIGHT = FULL_HEIGHT - 100;
+const FULL_HEIGHT = Dimensions.get('window').height;
+const FULL_WIDTH = Dimensions.get('window').width;
+const PANEL_HEIGHT = FULL_HEIGHT - 100;
 
-const STATUS = {
-    CLOSED: 0,
-    SMALL: 1,
-    LARGE: 2,
-};
-
-type SwipeablePanelProps = {
-    isActive: boolean,
-    onClose: () => void,
-    showCloseButton?: boolean,
-    fullWidth?: boolean,
-    noBackgroundOpacity?: boolean,
-    style?: Object,
-    onlyLarge?: boolean,
-    onlySmall?: boolean,
-    openLarge?: boolean,
-    smallPanelHeight?: number,
-    allowTouchOutside?: boolean,
-};
-
-type SwipeablePanelState = {
-    status: number,
-    showComponent: boolean,
-    canScroll: boolean,
-    pan: any,
-    orientation: 'portrait' | 'landscape',
-    deviceWidth: number,
-    deviceHeight: number,
-    panelHeight: number,
-};
-
-class SwipeablePanel extends Component<SwipeablePanelProps, SwipeablePanelState> {
-    pan: Animated.ValueXY;
-    isClosing: boolean;
-    _panResponder: any;
-    animatedValueY: number;
-    constructor(props: SwipeablePanelProps) {
-        super(props);
-        this.state = {
-            status: STATUS.CLOSED,
-            showComponent: false,
-            canScroll: false,
-            pan: new Animated.ValueXY({ x: 0, y: PANEL_HEIGHT }),
-            orientation: FULL_HEIGHT >= FULL_WIDTH ? 'portrait' : 'landscape',
-            deviceWidth: FULL_WIDTH,
-            deviceHeight: FULL_HEIGHT,
-            panelHeight: PANEL_HEIGHT,
-        };
-
-        this.pan = new Animated.ValueXY({ x: 0, y: PANEL_HEIGHT });
-        this.isClosing = false;
-        this.animatedValueY = 0;
-
-        this._panResponder = PanResponder.create({
-            onStartShouldSetPanResponder: () => true,
-            onPanResponderGrant: () => {
-                this.state.pan.setOffset({
-                    x: 0,
-                    y: this.animatedValueY,
-                });
-                this.state.pan.setValue({ x: 0, y: 0 });
-            },
-            onPanResponderMove: (evt, gestureState) => {
-                if (
-                    (this.state.status === 1 && Math.abs(this.state.pan.y._value) <= this.state.pan.y._offset) ||
-                    (this.state.status === 2 && this.state.pan.y._value > -1)
-                )
-                    this.state.pan.setValue({
-                        x: 0,
-                        y: this.state.status === STATUS.LARGE ? Math.max(0, gestureState.dy) : gestureState.dy,
-                    });
-            },
-            onPanResponderRelease: (evt, gestureState) => {
-                const { onlyLarge, onlySmall } = this.props;
-                this.state.pan.flattenOffset();
-
-                if (gestureState.dy === 0) this._animateTo(this.state.status);
-                else if (gestureState.dy < -100 || gestureState.vy < -0.5) {
-                    if (this.state.status === STATUS.SMALL) this._animateTo(onlySmall ? STATUS.SMALL : STATUS.LARGE);
-                    else this._animateTo(STATUS.LARGE);
-                } else if (gestureState.dy > 100 || gestureState.vy > 0.5) {
-                    if (this.state.status === STATUS.LARGE) this._animateTo(onlyLarge ? STATUS.CLOSED : STATUS.SMALL);
-                    else this._animateTo(0);
-                } else this._animateTo(this.state.status);
-            },
-            onMoveShouldSetPanResponder: () => {
-                return true;
-            },
-        });
-    }
-
-    componentDidMount = () => {
-        const { isActive, openLarge, onlyLarge, onlySmall } = this.props;
-
-        this.animatedValueY = 0;
-        this.state.pan.y.addListener((value: any) => (this.animatedValueY = value.value));
-
-        if (isActive) this._animateTo(onlySmall ? STATUS.SMALL : openLarge ? STATUS.LARGE : onlyLarge ? STATUS.LARGE : STATUS.SMALL);
-
-        Dimensions.addEventListener('change', this._onOrientationChange);
-    };
-
-    _onOrientationChange = () => {
-        const dimesions = Dimensions.get('screen');
-        FULL_HEIGHT = dimesions.height;
-        FULL_WIDTH = dimesions.width;
-
-        this.setState({
-            orientation: dimesions.height >= dimesions.width ? 'portrait' : 'landscape',
-            deviceWidth: FULL_WIDTH,
-            deviceHeight: FULL_HEIGHT,
-            panelHeight: PANEL_HEIGHT,
-        });
-
-        this.props.onClose();
-    };
-
-    componentDidUpdate(prevProps: SwipeablePanelProps, prevState: SwipeablePanelState) {
-        const { isActive, openLarge, onlyLarge, onlySmall } = this.props;
-        if (onlyLarge && onlySmall)
-            console.warn(
-                'Ops. You are using both onlyLarge and onlySmall options. onlySmall will override the onlyLarge in this situation. Please select one of them or none.'
-            );
-
-        if (prevProps.isActive !== isActive) {
-            if (isActive) {
-                this._animateTo(onlySmall ? STATUS.SMALL : openLarge ? STATUS.LARGE : onlyLarge ? STATUS.LARGE : STATUS.SMALL);
-            } else {
-                this._animateTo();
-            }
-        }
-
-        if (prevState.orientation !== this.state.orientation) this._animateTo(this.state.status);
-    }
-
-    _animateTo = (newStatus = 0) => {
-        const { smallPanelHeight } = this.props;
-        let newY = 0;
-
-        if (newStatus === STATUS.CLOSED) newY = PANEL_HEIGHT;
-        else if (newStatus === STATUS.SMALL)
-            newY = this.state.orientation === 'portrait' ? FULL_HEIGHT - (smallPanelHeight ?? 400) : FULL_HEIGHT / 3;
-        else if (newStatus === STATUS.LARGE) newY = 0;
-
-        this.setState({
-            showComponent: true,
-            status: newStatus,
-        });
-
-        Animated.spring(this.state.pan, {
-            toValue: { x: 0, y: newY },
-            tension: 80,
-            friction: 25,
-            useNativeDriver: true,
-            restDisplacementThreshold: 10,
-            restSpeedThreshold: 10,
-        }).start(() => {
-            if (newStatus === 0) {
-                this.props.onClose();
-                this.setState({
-                    showComponent: false,
-                });
-            } else
-                this.setState({
-                    canScroll: newStatus === STATUS.LARGE ? true : false,
-                });
-        });
-    };
-
-    render() {
-        const { showComponent, deviceWidth, deviceHeight, panelHeight } = this.state;
-        const { noBackgroundOpacity, style, onClose, allowTouchOutside, closeOnTouchOutside } = this.props;
-
-        return showComponent ? (
-            <Animated.View
-                style={[
-                    SwipeablePanelStyles.background,
-                    {
-                        backgroundColor: noBackgroundOpacity ? 'rgba(0,0,0,0)' : 'rgba(0,0,0,0.5)',
-                        height: allowTouchOutside ? 'auto' : deviceHeight - 20,
-                        width: deviceWidth,
-                    },
-                ]}
-            >
-                {closeOnTouchOutside && (
-                    <TouchableWithoutFeedback onPress={() => onClose()}>
-                        <View
-                            style={[
-                                SwipeablePanelStyles.background,
-                                {
-                                    width: deviceWidth,
-                                    backgroundColor: 'rgba(0,0,0,0)',
-                                    height: allowTouchOutside ? 'auto' : deviceHeight,
-                                },
-                            ]}
-                        />
-                    </TouchableWithoutFeedback>
-                )}
-                <Animated.View
-                    style={[
-                        SwipeablePanelStyles.panel,
-                        {
-                            width: this.props.fullWidth ? deviceWidth : deviceWidth - 50,
-                            height: panelHeight,
-                        },
-                        { transform: this.state.pan.getTranslateTransform() },
-                        style,
-                    ]}
-                >
-                    {!this.state.canScroll ? (
-                        <TouchableHighlight>
-                            <React.Fragment>{this.props.children}</React.Fragment>
-                        </TouchableHighlight>
-                    ) : (
-                        this.props.children
-                    )}
-                </Animated.View>
-            </Animated.View>
-        ) : null;
-    }
-}
-
-const SwipeablePanelStyles = StyleSheet.create({
-    background: {
+const styles = StyleSheet.create({
+    mask: {
         position: 'absolute',
+        height: FULL_HEIGHT,
+        width: FULL_WIDTH,
         zIndex: 1,
         bottom: 0,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0,0,0,0.5)',
+        backgroundColor: '#000',
     },
     panel: {
         position: 'absolute',
+        zIndex: 2,
         height: PANEL_HEIGHT,
-        width: FULL_WIDTH - 50,
-        transform: [{ translateY: FULL_HEIGHT - 100 }],
-        display: 'flex',
-        flexDirection: 'column',
-        backgroundColor: 'white',
+        width: '100%',
         bottom: 0,
+        backgroundColor: GlobalStyles.color.DARKWHITE,
         borderTopLeftRadius: 8,
         borderTopRightRadius: 8,
-        borderBottomLeftRadius: 0,
-        borderBottomRightRadius: 0,
-        overflow: 'hidden',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.18,
-        shadowRadius: 1.0,
-        elevation: 1,
-        zIndex: 2,
-    },
-    scrollViewContentContainerStyle: {
-        width: '100%',
     },
 });
 
-export default SwipeablePanel;
+export default function SwipeablePanel(props) {
+    const { children, isActive, title, onClose } = props;
+    const [isCloseButtonPressed, setIsCloseButtonPressed] = useState(false);
+    const animatedOpacity = useRef(new Animated.Value(0)).current;
+    const animatedTranslateY = useRef(new Animated.Value(FULL_HEIGHT)).current;
+    const animatedMaskStyle = {
+        opacity: animatedOpacity,
+    };
+    const animatedPanelStyle = {
+        transform: [{ translateY: animatedTranslateY }],
+    };
+    const maskStyle = [styles.mask, animatedMaskStyle];
+    const panelStyle = [styles.panel, animatedPanelStyle];
+    const isPanelExpanded = isActive && !isCloseButtonPressed;
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
+        handlePressClose();
+        return true;
+    });
+    const handlePressClose = () => setIsCloseButtonPressed(true);
+    const close = () => {
+        if (!isPanelExpanded) {
+            backHandler.remove();
+            onClose();
+        }
+    };
+
+    useEffect(() => {
+        Animated.timing(animatedOpacity, {
+            toValue: isPanelExpanded ? 0.5 : 0,
+            duration: 200,
+            useNativeDriver: true,
+        }).start();
+        Animated.spring(animatedTranslateY, {
+            toValue: isPanelExpanded ? 0 : FULL_HEIGHT,
+            tension: 80,
+            friction: 25,
+            restDisplacementThreshold: 10,
+            restSpeedThreshold: 10,
+            useNativeDriver: true,
+        }).start(close);
+    }, [isActive, isCloseButtonPressed]);
+
+    return (
+        <>
+            <Animated.View style={maskStyle} />
+            <Animated.View style={panelStyle}>
+                <TitleBar theme="light" title={title} onClose={handlePressClose} />
+                {children}
+            </Animated.View>
+        </>
+    );
+}
+
+SwipeablePanel.propsTypes = {
+    children: PropTypes.node,
+    isActive: PropTypes.bool.isRequired,
+    title: PropTypes.string,
+    onClose: PropTypes.func.isRequired,
+};
