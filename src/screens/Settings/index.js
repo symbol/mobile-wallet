8f08ce95608a7836a06dd85f4fd43b8ff74e44bc
Oleg Makarenko <olegomm@gmail.com>
2022-10-24 19:21:49 +0300

    [wallet/mobile] feat: add version info

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,44 +1,55 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import translate from '@src/locales/i18n';
 import styles from './Settings.styl';
 import { Router } from '@src/Router';
-
 import {
+	Section,
 	SymbolPageView,
-	SettingsCurrencySelector,
 	SettingsLanguageSelector,
-	SettingsNotificationSelector,
 	SettingsPasscodeSelector,
 	SettingsBackup,
 	SettingsAboutNem,
 	SettingsLogout,
-	SettingsNodeSelector
+	SettingsNodeSelector,
+	Text
 } from '@src/components';
 import SettingsNIS1NodeSelector from "@src/components/settings/SettingsNIS1NodeSelector";
+import packageJson from '../../../package.json';
+import symbolSdkPackageJson from 'symbol-sdk/package.json';
 
 export default class Settings extends Component {
     render() {
 		const isLoading = false;
         return (
-            <SymbolPageView
-				theme="dark2"
-				isFade
-				isLoading={isLoading}
-				title={ translate('Settings.title') }
-				icon="settings"
-				onBack={() => {Router.goBack(this.props.componentId)}}
-			>
-				<View style={styles.listContainer}>
-					<SettingsLanguageSelector />
-					<SettingsNIS1NodeSelector />
-					<SettingsPasscodeSelector {...this.props} />
-					<SettingsNodeSelector />
-					<SettingsBackup {...this.props} />
-					<SettingsAboutNem />
-					<SettingsLogout {...this.props} />
-				</View>
-            </SymbolPageView>
+			<>
+				<SymbolPageView
+					theme="dark2"
+					isFade
+					isLoading={isLoading}
+					title={ translate('Settings.title') }
+					icon="settings"
+					onBack={() => {Router.goBack(this.props.componentId)}}
+				>
+					<View style={styles.listContainer}>
+						<SettingsLanguageSelector />
+						<SettingsNIS1NodeSelector />
+						<SettingsPasscodeSelector {...this.props} />
+						<SettingsNodeSelector />
+						<SettingsBackup {...this.props} />
+						<SettingsAboutNem />
+						<SettingsLogout {...this.props} />
+					</View>
+				</SymbolPageView>
+				<Section type="form" style={styles.versions}>
+					<Text style={styles.appVersion}>
+						Release: v{packageJson.version}
+					</Text>
+					<Text style={styles.sdkVersion}>
+						Symbol SDK: v{symbolSdkPackageJson.version}
+					</Text>
+				</Section>
+			</>
         );
     }
 }
